2c821e43375242fa2ac58706c85fd6191b3f4fde
cmzuo11 <cmzuo13@163.com>
2021-01-28 09:58:03 +0800

    Update Main_SNARE_seq.py

--- Main_SNARE_seq.py
+++ Main_SNARE_seq.py
@@ -25,15 +25,15 @@ def train_with_argas( args ):
     args.cluster1   =  args.cluster2   =  4
     args.lr1        =  0.01
     args.flr1       =  0.001
-    args.lr2        =  0.005
-    args.flr2       =  0.0005
+    args.lr2        =  0.002
+    args.flr2       =  0.0002
 
-    args.workdir  =  './Example_test/'
-    args.outdir   =  './Example_test/'
+    args.workdir    =  './Example_test/'
+    args.outdir     =  './Example_test/'
 
-    args.File1    =  os.path.join( args.workdir, 'scRNA_seq_SNARE.tsv' ) 
-    args.File3    =  os.path.join( args.workdir, 'cell_metadata.txt' ) 
-    args.File2_1  =  os.path.join( args.workdir, 'scATAC_seq_SNARE.txt' ) 
+    args.File1      =  os.path.join( args.workdir, 'scRNA_seq_SNARE.tsv' ) 
+    args.File3      =  os.path.join( args.workdir, 'cell_metadata.txt' ) 
+    args.File2_1    =  os.path.join( args.workdir, 'scATAC_seq_SNARE.txt' ) 
 
     adata, _, adata2, train_index, test_index, label_ground_truth, _ = read_dataset( File1 = args.File1,
                                                                                      File3 = args.File3,
@@ -93,12 +93,12 @@ def train_with_argas( args ):
     torch.save(model, os.path.join( args.outdir, 'model_DCCA.pt' ) )
 
     result1, result2 = model( total_loader )
-    latent_z1 = result1["latent_z1"].data.cpu().numpy()
-    norm_x1   = result1["norm_x"].data.cpu().numpy()
-    recon_x1  = result1["recon_x"].data.cpu().numpy()
+    latent_z1        = result1["latent_z1"].data.cpu().numpy()
+    norm_x1          = result1["norm_x"].data.cpu().numpy()
+    recon_x1         = result1["recon_x"].data.cpu().numpy()
 
-    latent_z2 = result2["latent_z1"].data.cpu().numpy()
-    norm_x2   = result2["norm_x"].data.cpu().numpy()
+    latent_z2        = result2["latent_z1"].data.cpu().numpy()
+    norm_x2          = result2["norm_x"].data.cpu().numpy()
 
     if latent_z1 is not None:
         imputed_val  = pd.DataFrame( latent_z1, index= adata.obs_names ).to_csv( os.path.join( args.outdir, 'scRNA-latent.csv' ) ) 
@@ -114,12 +114,10 @@ def train_with_argas( args ):
         norm_x1_1    = pd.DataFrame( norm_x2, columns =  adata2.var_names, 
                                      index= adata2.obs_names ).to_csv( os.path.join( args.outdir, 'scATAC-norm.csv' ) )
 
-
 if __name__ == "__main__":
 
-    parser        =  parameter_setting()
-    args          =  parser.parse_args()
+    parser  =  parameter_setting()
+    args    =  parser.parse_args()
 
     train_with_argas(args)
-
 
